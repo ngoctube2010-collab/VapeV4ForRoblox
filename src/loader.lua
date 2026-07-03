ad66e7b6e8351b1c934d86aa325be17ea99dd764
Tu9809393 <ngoctube2010@gmail.com>
2026-07-03 12:24:57 +0700

    Update loader.lua

--- src/loader.lua
+++ src/loader.lua
@@ -42,7 +42,7 @@ end
 
 if not shared.VapeDeveloper then
 	local _, subbed = pcall(function()
-		return game:HttpGet('https://github.com/7GrandDadPGN/VapeCompiled')
+		return game:HttpGet('https://github.com/ngoctube2010-collab/VapeCompiled')
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
